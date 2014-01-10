91e62685d9373708e53f491bf3fc93daff01ec65
PlatinumJesus <cipettaro@gmail.com>
2014-01-10 08:44:54 +0100

    / ‘ubuntuCustomizer.sh’

--- ubuntuCustomizer.sh
+++ ubuntuCustomizer.sh
@@ -90,6 +90,7 @@ function quit() {
 function welcome() {
 [ ! -f $status ] && touch $status
 dialog --title 'Welcome' --msgbox "Hello! This script will allow you to personalize your Ubuntu 12.04 iso installation.\nPlease remember that this is a student work and uses root functionalities.\nPlease make sure you have:\n-a working internet connection\n-at least 3gb of space in ${HOME}\nThe author does not take any responsability on unexpected behaviours!\n\n\nCheers and press ENTER to continue" $hght $wdth
+
 echo "getiso" >> $status
 }
 
